e7438cdd0a472b43a3c2424ae32076fa1fed03f5
Derya Kaygisiz <derya.kaygisiz@outlook.com>
2021-05-23 16:00:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  get diameter() {
+   return this.radius * 2
+ }
+
+ set diameter(newDiameter) {
+   this.radius = newDiameter / 2;
+ }
+
+
+ get circumference() {
+  return this.sideLength * this.sideLength;
+}
+
+set area(newCircumference) {
+  this.sideLength = Math.sqrt(newCircumference);
+}
+
+
+  get area() {
+   return this.sideLength * this.sideLength;
+ }
+
+ set area(newArea) {
+   this.sideLength = Math.sqrt(newArea);
+ }
+
+}
